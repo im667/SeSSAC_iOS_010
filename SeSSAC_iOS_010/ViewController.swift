bd379db2b1d0c2db95e99755065dbbf8340ebe9b
kwon <koh9037@gmail.com>
2021-10-12 14:32:37 +0900

    Initial Commit

---
+++ SeSSAC_iOS_010/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSSAC_iOS_010
+//
+//  Created by mac on 2021/10/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
